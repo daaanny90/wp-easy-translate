c4100ed99bedaf22d9ee11630b81db4d7b9f7519
spina <Danny.Spina@3m5.de>
2020-06-04 11:46:01 +0200

    Add first elements to sidebar

--- sidebar/js/plugin-sidebar.js
+++ sidebar/js/plugin-sidebar.js
@@ -1,18 +1,23 @@
 (function (wp) {
-    let registerPlugin = wp.plugins.registerPlugin;
-    let PluginSidebar = wp.editPost.PluginSidebar;
-    let el = wp.element.createElement;
-    let Button = wp.components.Button;
+    // get all required elements from wordpress
+    const registerPlugin = wp.plugins.registerPlugin;
+    const PluginSidebar = wp.editPost.PluginSidebar;
+    const el = wp.element.createElement;
+    const {Button, SelectControl, CheckboxControl} = wp.components;
     const {select, dispatch} = wp.data;
 
-    // requests for translation
-    let translateContent = new XMLHttpRequest();
-    let translateTitle = new XMLHttpRequest();
+    // requests to yandex
+    const translateContent = new XMLHttpRequest();
+    const translateTitle = new XMLHttpRequest();
+    const getLanguages = new XMLHttpRequest();
 
     // get saved options
     const apiKey = options.easytranslate_field_api;
-    const langFrom = options.easytranslate_lang_1;
-    const langTo = options.easytranslate_lang_2;
+    let langFrom = options.easytranslate_lang_1;
+    let langTo = options.easytranslate_lang_2;
+
+    // Language list functionality
+    let overrideSettings;
 
     // requests for title and content (for the content the blocks must be reset)
     translateTitle.onload = function () {
@@ -33,12 +38,36 @@
 
             dispatch('core/editor').editPost({content: translatedData});
             dispatch('core/block-editor').resetBlocks(wp.blocks.parse(translatedData));
+            console.log(languageList)
 
         } else {
             console.log(translateContent);
         }
     };
 
+    // get available languages
+    const languageList = [];
+    getLanguages.onload = function () {
+        if (getLanguages.status >= 200 && getLanguages.status < 300) {
+            let response = JSON.parse(getLanguages.response);
+            let languages = response.langs;
+            for (let key of Object.keys(languages)) {
+                languageList.push({
+                    label: languages[key],
+                    value: key
+                });
+            }
+        } else {
+            console.log(translateContent);
+        }
+    };
+    getLanguages.open('GET', 'https://translate.yandex.net/api/v1.5/tr.json/getLangs?' +
+        'key=' + apiKey +
+        '&ui=en'
+    );
+    getLanguages.send();
+
+    // render the sidebar
     registerPlugin('easytranslate-sidebar', {
         render: function () {
             return el(PluginSidebar,
@@ -47,12 +76,12 @@
                     icon: 'translation',
                     title: 'Easy Translate',
                 },
+
+                // translate button section
                 el('div',
-                    {className: 'easytranslate-sidebar-content'},
+                    {className: 'easytranslate-sidebar-button'},
                     el(Button, {
                         isPrimary: true,
-                        label: 'Meta Block Field',
-                        value: 'Initial value',
                         onClick: function () {
                             const titleRaw = select('core/editor').getEditedPostAttribute('title');
                             const title = encodeURI(titleRaw);
@@ -76,6 +105,41 @@
                             translateTitle.send();
                         },
                     }, "Translate your content")
+                ),
+
+                // language section
+                el('div',
+                    {className: 'easytranslate-sidebar-languages'},
+
+                    // choose if override the main settings or choose new languages for this post
+                    el(CheckboxControl, {
+                        heading: 'User',
+                        label: 'Is Autor',
+                        help: 'Is the user a author or not?',
+                        onChange: function (checked) {
+                            overrideSettings = checked;
+                        }
+                    }),
+
+                    // select to choose the language from
+                    el(SelectControl, {
+                        label: 'Language 1',
+                        value: 'value',
+                        options: [languageList],
+                        onChange: function (value) {
+                            console.log('language 1: ' + value)
+                        }
+                    }),
+
+                    // language to choose the language to
+                    el(SelectControl, {
+                        label: 'Language 1',
+                        value: 'value',
+                        options: [languageList],
+                        onChange: function (value) {
+                            console.log('language 2: ' + value)
+                        }
+                    })
                 )
             );
         },
